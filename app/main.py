56ecaf4397bc2338a4cd29653d0348ce0f6b58c3
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-15 20:27:07 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -40,7 +40,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
